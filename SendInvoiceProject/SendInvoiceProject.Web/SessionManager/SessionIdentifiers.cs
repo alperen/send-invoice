bbe26799706e15cb7504dbdaf9f3aebef97eaadc
Alperen Turkoz <alperentrkz@gmail.com>
2021-04-10 11:38:18 +0300

    add identifiers

---
+++ SendInvoiceProject/SendInvoiceProject.Web/SessionManager/SessionIdentifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendInvoiceProject.Web.SessionManager
+{
+    public static class SessionIdentifiers
+    {
+        public const string User = "USER";
+    }
+}
---
+++ SendInvoiceProject/SendInvoiceProject.Web/SessionManager/SessionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendInvoiceProject.Web.SessionManager
+{
+    public class SessionManager
+    {
+        public Business.User getUser()
+        {
+            var userSession = (Business.User) HttpContext.Current.Session[SessionIdentifiers.User];
+
+            return userSession;
+        }
+    }
+}
